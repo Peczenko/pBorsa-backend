fee6c5445342a66bc27ea00b3bfdb741fab189f2
arseni <r.seny.martysiuk@gmail.com>
2026-01-01 18:51:41 +0100

    feat: refactor trade execution logics

---
+++ pborsa-temporal-api/src/main/java/com/pborsa/api/temporal/activity/OrderStatusUpdateActivities.java
@@ -0,0 +1,17 @@
+package com.pborsa.api.temporal.activity;
+
+import com.pborsa.api.domain.dto.trading.OrderStatusUpdateRequest;
+import io.temporal.activity.ActivityInterface;
+import io.temporal.activity.ActivityMethod;
+
+import java.util.UUID;
+
+/**
+ * Temporal activities interface for updating order status in the API app.
+ */
+@ActivityInterface
+public interface OrderStatusUpdateActivities {
+
+    @ActivityMethod
+    void updateOrderStatus(OrderStatusUpdateRequest request);
+}
