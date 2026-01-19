757bc3d99a961b4981b8a0bf6c3cc9a59085725d
arseni <r.seny.martysiuk@gmail.com>
2026-01-19 16:15:38 +0100

    fix: remove cord policy

--- pborsa-api/src/main/java/com/pborsa/api/config/WebSecurityConfig.java
+++ pborsa-api/src/main/java/com/pborsa/api/config/WebSecurityConfig.java
@@ -29,7 +29,7 @@ public class WebSecurityConfig {
         FirebaseAuthenticationFilter firebaseFilter = new FirebaseAuthenticationFilter(firebaseAuth);
 
         http
-                .cors(withDefaults())
+                .cors(AbstractHttpConfigurer::disable)
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(requests -> requests
